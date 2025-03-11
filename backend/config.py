b6fd95663773eabe2ee0e576dcc30bb9d7dbdc7d
Arham Shah <shaharhamrahul@gmail.com>
2025-03-11 17:03:42 -0400

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'acara-development-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://localhost/acara'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET') or 'your-super-secret-jwt-token'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET')
